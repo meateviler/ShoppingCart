2b12323a129a60c5235858a269f7171d224c1bdc
meateviler <meateviler@msn.com>
2017-06-21 17:48:56 +0800

    add first test

---
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShoppingCart
+{
+	public class ShoppingCart
+	{
+		public ShoppingCart()
+		{
+		}
+
+		public void AddBook(string p1, int p2)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetPrice()
+		{
+			return 0;
+		}
+	}
+}
---
+++ ShoppingCart/ShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ShoppingCart.Tests
+{
+	[TestFixture()]
+	public class ShoppingCartTests
+	{
+		private Dictionary<int, string> _bookName = new Dictionary<int, string>()
+		{
+			{0,"HarryPoter1"},
+			{1,"HarryPoter2"},
+			{2,"HarryPoter3"},
+			{3,"HarryPoter4"},
+			{4,"HarryPoter5"}
+		};
+
+		[TestCase(new[] { 1, 0, 0, 0, 0 }, ExpectedResult = 100, TestName = "BuyOneShouldBe100")]
+		public int ShoppingCartTest(int[] bookArray)
+		{
+			var target = new ShoppingCart();
+			for (var i = 0; i < 5; i++)
+			{
+				if (bookArray[i] > 0)
+				{
+					target.AddBook(_bookName[i], bookArray[i]);
+				}
+			}
+			return target.GetPrice();
+		}
+	}
+}
