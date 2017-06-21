038918e517462ef705ac611af30030448fbac854
meateviler <meateviler@msn.com>
2017-06-21 18:06:13 +0800

    Let 2nd test case pass
    Buy 2 different book , price should be 190

--- ShoppingCart/ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -1,17 +1,24 @@
-﻿namespace ShoppingCart
+﻿using System.Collections.Generic;
+
+namespace ShoppingCart
 {
 	public class ShoppingCart
 	{
+		private readonly Dictionary<string, int> _shoppingProduct = new Dictionary<string, int>();
+
 		public ShoppingCart()
 		{
 		}
 
-		public void AddBook(string p1, int p2)
+		public void AddBook(string productName, int amount)
 		{
+			_shoppingProduct.Add(productName, amount);
 		}
 
 		public int GetPrice()
 		{
+			if (_shoppingProduct.Count == 2)
+				return (int)(_shoppingProduct.Count * 100 * 0.95);
 			return 100;
 		}
 	}
